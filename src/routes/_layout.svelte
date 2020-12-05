ffbdf15e36ad93009ec3a7b00a6791e17da77fa0
AguzzTN54 <gusagusnanang@gmail.com>
2020-12-06 06:38:29 +0700

    Setup @smui component

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,22 +1,67 @@
 <script>
-	import Nav from '../components/Nav.svelte';
+  import '@splidejs/splide/dist/css/themes/splide-skyblue.min.css';
+  import { onMount, setContext } from 'svelte';
+  import { writable } from 'svelte/store';
+  import { stores } from '@sapper/app';
+  import { DenseFixedAdjust } from '@smui/top-app-bar';
+  import LinearProgress from '@smui/linear-progress';
+  import Appbar from '../components/Appbar.svelte';
 
-	export let segment;
+  const { preloading } = stores();
+  
+  let mobile = false;
+  const toggleResponsive = () => { mobile = (window.innerWidth < 750); };
+  onMount(toggleResponsive);
+
+  const responsive = writable(mobile);
+  $: responsive.set(mobile);
+  setContext('responsive', responsive);
+
+  export let segment;
+  $: console.log(segment);
 </script>
 
 <style>
-	main {
-		position: relative;
-		max-width: 56em;
-		background-color: white;
-		padding: 2em;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
+  :global(.splide__pagination) {
+    bottom: 0;
+  }
+  :global(.splide__pagination button.is-active) {
+    background-color: var(--mdc-theme-primary);
+  }
+  :global(.splide__arrow) {
+    background-color: #fff;
+    border-radius: 100%;
+    height: 35px;
+    width: 35px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    opacity: 0.5;
+    transition: all .5s;
+  }
+  :global(.splide__arrow:hover) {
+    opacity: 1;
+  }
+
+  :global(.splide__arrow svg) {
+    fill: var(--mdc-theme-primary) !important;
+    width: 70% !important;
+  }
 </style>
 
-<Nav {segment}/>
+<svelte:window on:resize={toggleResponsive} />
+{#if $preloading}
+  <LinearProgress indeterminate class="preloader"/>
+{/if}
 
-<main>
-	<slot></slot>
-</main>
+{#if segment === 'auth'}
+  <main>
+    <slot />
+  </main>
+
+{:else}
+  <Appbar {segment}/>
+  <main use:DenseFixedAdjust>
+    <slot />
+  </main>
+{/if}
