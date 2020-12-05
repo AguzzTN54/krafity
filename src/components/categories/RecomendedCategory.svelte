083b0362b4318d18d31c7544717d591b37a1563a
AguzzTN54 <gusagusnanang@gmail.com>
2020-12-06 06:47:54 +0700

    Create Categories List Component

---
+++ src/components/categories/RecomendedCategory.svelte
@@ -0,0 +1,101 @@
+<style>
+  /* .recomended {
+    border: 1px solid #ccc;
+    padding: 10px;
+    margin: 0 15px;
+    border-radius: 10px;
+  } */
+
+  h2 {
+    font-size: 1.3rem;
+    text-align: center;
+  }
+  
+  .list {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    flex-wrap: wrap;
+    width: 100%;
+  }
+
+  .item {
+    width:110px;
+    height:110px;
+    overflow: hidden;
+    border-radius: 10px;
+    position: relative;
+    color: #fff;
+    margin: 5px;
+  }
+  .item::after {
+    content: '';
+    display: block;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    background-image: linear-gradient(to top, rgba(0,0,0,0.65), rgba(0,0,0,0));
+  }
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  .caption {
+    display: block;
+    position: absolute;
+    bottom:0;
+    left:0;
+    color: #fff;
+    z-index: +1;
+    font-size: .7rem;
+    padding: 5px 10px;
+  }
+</style>
+
+<div class="recomended">
+  <h2> Kategori Pilihan </h2>
+
+  <div class="list">
+    <a href="/"> 
+      <div class="item">
+        <img src="/images/pic2.jpg" alt="pick" />
+        <div class="caption">
+          <h3> Souvenir</h3>
+        </div>
+      </div>
+    </a>
+
+    <a href="/"> 
+      <div class="item">
+        <img src="/images/pic2.jpg" alt="pick" />
+        <div class="caption">
+          <h3> Souvenir</h3>
+        </div>
+      </div>
+    </a>
+
+    <a href="/"> 
+      <div class="item">
+        <img src="/images/pic2.jpg" alt="pick" />
+        <div class="caption">
+          <h3> Souvenir</h3>
+        </div>
+      </div>
+    </a>
+
+    <a href="/"> 
+      <div class="item">
+        <img src="/images/pic3.jpg" alt="pick" />
+        <div class="caption">
+          <h3> Souvenir</h3>
+        </div>
+      </div>
+    </a>
+
+  </div>
+</div>
---
+++ src/components/categories/TopbarCategoryList.svelte
@@ -0,0 +1,65 @@
+<script>
+  import data from '../../dummy/categories.json';
+
+  const { categories } = data;
+</script>
+
+<style>
+  .top-bar-categories {
+    width: 100%;
+    overflow-x: auto;
+    padding: 2PX 10px;
+    border-width: 1px 0;
+    border-color: #ccc;
+    border-style: solid;
+  }
+  .top-bar-categories::-webkit-scrollbar {
+    display: none;
+  }
+  .top-bar-categories:hover::-webkit-scrollbar {
+    display: unset;
+  }
+
+  .categories {
+    display: flex;
+    justify-content: center;
+    white-space: nowrap;
+  }
+
+  .category-item {
+    padding: 0 5px;
+    margin: 0 3px;
+    display: inline-block;
+    position: relative;
+    text-decoration:none;
+    text-transform: capitalize;
+  }
+  .category-item::after {
+    content: '';
+    display: block;
+    height: 80%;
+    position: absolute;
+    top:50%;
+    right: -3px;
+    transform: translateY(-50%);
+    border-right: 1px solid #ccc;
+  }
+  .category-item:last-child::after {
+    content: none;
+  }
+</style>
+
+<div class="top-bar-categories">
+  <div class="categories">
+    {#if categories.length > 0}
+      {#each categories as category}
+        <a href="blog/categories/{category}" class="category-item">
+          {category}
+        </a>
+      {/each}
+    {:else}
+      <span>no Data found</span>
+    {/if}
+
+  </div>
+</div>
