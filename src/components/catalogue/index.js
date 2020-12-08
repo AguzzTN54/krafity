f29919ff0e5727680e9e993c21a93ac95bc32838
AguzzTN54 <gusagusnanang@gmail.com>
2020-12-08 13:35:31 +0700

    Update product component

--- src/components/catalogue/index.js
+++ src/components/catalogue/index.js
@@ -1,4 +1,5 @@
 import ProductComponent1 from './ProductComponent1.svelte';
+import ProductComponent2 from './ProductComponent2.svelte';
 import ProductLandscape from './ProductLandscape.svelte';
 
-export { ProductComponent1, ProductLandscape };
+export { ProductComponent1, ProductComponent2, ProductLandscape };
