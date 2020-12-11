a6604c1b369513e0ed75686da705eef648b5d3d5
AguzzTN54 <gusagusnanang@gmail.com>
2020-12-11 23:44:49 +0700

    Configure components fo consume data from REST

--- src/components/posts/MainPostList.svelte
+++ src/components/posts/MainPostList.svelte
@@ -1,6 +1,6 @@
 <script>
   import axios from 'axios';
-  import Skeleton from '../utils/Skeleton.svelte';
+  import HPostComponent from './HPostComponent.svelte';
 
   const getData = async () => {
     try {
@@ -16,69 +16,6 @@
 </script>
 
 <style>
-  .main-item {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: row-reverse;
-    padding: 10px;
-    margin: 10px 0;
-  }
-
-  figure {
-    width: 120px;
-    height:120px;
-    margin: 0;
-  }
-
-  .caption {
-    flex-basis: calc(100% - 120px);
-    flex-grow: 1;
-  }
-
-  h3 {
-    font-size: large;
-    font-weight: bold;
-  }
-
-  .user a, .link {
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-  }
-  .user img, :global(.avatar) {
-    width: 20px;
-    height: 20px;
-    margin-right: 5px;
-    border-radius: 100%;
-    overflow: hidden;
-  }
-
-  .info {
-    color: #666;
-    padding: 0 10px;
-    display: inline-block;
-    position: relative;
-    font-weight: lighter;
-    font-size: .8rem;
-  }
-  .info::before {
-    content: '';
-    width:3px;
-    height:3px;
-    border-radius: 100%;
-    position: absolute;
-    left:0;
-    top: 50%;
-    transform: translateY(-50%);
-    background-color: #888;
-  }
-  p {
-    margin-bottom: -3px;
-    margin-top: -2px;
-    margin-right: 5px;
-  }
-
   .load-more {
     color: var(--mdc-theme-primary);
     border: 1px solid var(--mdc-theme-primary);
@@ -100,51 +37,12 @@
 
     {#await tutorials}
       {#each [1, 2, 3, 4, 5] as i (i)}
-        <div class="main-item" id="skln-{i}">
-          <figure>
-            <Skeleton width="100%" height="100%" />
-          </figure>
-          <div class="caption">
-            <div class="user">
-              <span class="link">
-                <Skeleton className="avatar"/>
-                <Skeleton width="70px" height="12px"/>
-              </span>
-            </div>
-
-            <Skeleton width="180px" height="18px"/>
-            <p class="short-desc text-overflow">
-              <Skeleton width="80%" height="8px"/>
-              <Skeleton width="45%" height="8px"/>
-            </p>
-            <span class="info"><Skeleton width="50px" height="10px"/></span>
-            <span class="info"><Skeleton width="50px" height="10px"/></span>
-          </div>
-        </div>
+        <HPostComponent skeleton />
       {/each}
 
-    {:then data} 
-      {#each data as { id, title, slug, user, thumbnail, content, views } (id)}
-        <div class="main-item">
-          <figure>
-            <img class="lazyload" src="/assets/images/thumbnail.svg" data-src={thumbnail} alt={title}>
-          </figure>
-          <div class="caption">
-            <div class="user">
-              <a href="/user/siapa-ya">
-                <img class="lazyload" src="/assets/images/thumbnail.svg" data-src={user.avatar} alt="Avatar">
-                <span>{user.name}</span>
-              </a>
-            </div>
-
-            <h3 class="text-overflow"><a href="/tutorial/{slug}" rel="prefetch">{title}</a></h3>
-            <p class="short-desc text-overflow">
-              {content.replace(/(<([^>]+)>)/ig, '').substring(0, 200)}
-            </p>
-            <span class="info">{views} Views</span>
-            <span class="info">1.5 jam</span>
-          </div>
-        </div>
+    {:then tutor} 
+      {#each tutor as data (data.id)}
+        <HPostComponent data={data} />
       {/each}
 
     {/await}
@@ -152,6 +50,6 @@
   </div>
 
   <div class="text-center">
-    <a href="/" class="load-more"> Lihat Semua </a>
+    <a href="/" class="load-more"> Load More </a>
   </div>
 </div>
