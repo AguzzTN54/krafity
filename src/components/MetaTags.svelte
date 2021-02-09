4c400d4fffd77fc26e48d3f70f4b6c36a786c4f1
AguzzTN54 <gusagusnanang@gmail.com>
2021-02-10 00:17:53 +0700

    Update MetaTags Component

--- src/components/MetaTags.svelte
+++ src/components/MetaTags.svelte
@@ -6,6 +6,7 @@
   export let path = '';
   export let thumbnail = '';
 
+  // prettier-ignore
   const {
     TITLE, SITE_URL, DESCRIPTION, KEYWORDS,
   } = CONFIG;
@@ -13,21 +14,20 @@
   const thumb = thumbnail || `${SITE_URL}/assets/images/screen.png`;
 </script>
 
-<title> {title || TITLE} </title>
-<meta name="title" content="{title || TITLE}"/>
-<meta name="description" content='{description || DESCRIPTION}'/>
-<meta name="keywords" content="{KEYWORDS}"/>
+<meta name="title" content={title || TITLE} />
+<meta name="description" content={description || DESCRIPTION} />
+<meta name="keywords" content={KEYWORDS} />
 
-<link rel="fluid-icon" href="{thumb}" title="{title || TITLE}"/>
+<link rel="fluid-icon" href={thumb} title={title || TITLE} />
 
-<meta property="og:type" content="website"/>
-<meta property="og:url" content="{SITE_URL}{path}"/>
-<meta property="og:title" content="{title || TITLE}"/>
-<meta property="og:description" content='{description || DESCRIPTION}'/>
-<meta property="og:image" content="{thumb}"/>
+<meta property="og:type" content="website" />
+<meta property="og:url" content="{SITE_URL}{path}" />
+<meta property="og:title" content={title || TITLE} />
+<meta property="og:description" content={description || DESCRIPTION} />
+<meta property="og:image" content={thumb} />
 
-<meta property="twitter:card" content="summary_large_image"/>
-<meta property="twitter:url" content="{SITE_URL}{path}"/>
-<meta property="twitter:title" content="{title || TITLE}"/>
-<meta property="twitter:description" content='{description || DESCRIPTION}'/>
-<meta property="twitter:image" content="{thumb}"/>
+<meta property="twitter:card" content="summary_large_image" />
+<meta property="twitter:url" content="{SITE_URL}{path}" />
+<meta property="twitter:title" content={title || TITLE} />
+<meta property="twitter:description" content={description || DESCRIPTION} />
+<meta property="twitter:image" content={thumb} />
